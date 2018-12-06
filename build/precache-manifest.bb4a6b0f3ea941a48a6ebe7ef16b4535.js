f0c40d6156ee711a9f36a926f60d2772db4d99b8
Martin Gorupić <37804840+MARTIN111Zg@users.noreply.github.com>
2018-12-06 03:17:46 +0100

    Initial commit

---
+++ build/precache-manifest.bb4a6b0f3ea941a48a6ebe7ef16b4535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "819c0e200f59db206cba",
+    "url": "/static/js/main.819c0e20.chunk.js"
+  },
+  {
+    "revision": "b65ef193d60c4de6b0a4",
+    "url": "/static/js/1.b65ef193.chunk.js"
+  },
+  {
+    "revision": "819c0e200f59db206cba",
+    "url": "/static/css/main.69253604.chunk.css"
+  },
+  {
+    "revision": "b65ef193d60c4de6b0a4",
+    "url": "/static/css/1.e59d9b9b.chunk.css"
+  },
+  {
+    "revision": "7b288e4b1af19a517f04165ba1448231",
+    "url": "/index.html"
+  }
+];
